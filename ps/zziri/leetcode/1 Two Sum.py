1abcf84d8d2c53d3c14863eb5dd0327d1274ed72
zziri <jihzang@naver.com>
2020-12-24 00:49:27 +0900

    중복 파일 삭제

--- ps/zziri/leetcode/1 Two Sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        limit = len(nums)
-        for i in range(limit):
-            for j in range(i+1, limit):
-                if nums[i] + nums[j] == target:
-                    return [i, j]

fb9137802c8c4c693f7baded3f3a502f7f246fcb
zziri <jihzang@naver.com>
2021-02-22 04:20:07 +0900

    Valid Palindrome.py: refactoring

--- ps/zziri/leetcode/top-interview-questions-easy/String/Valid Palindrome.py
+++ ps/zziri/leetcode/top-interview-questions-easy/String/Valid Palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         def convert(s):
-            ret = ''
+            ret = list()
             for c in s:
                 code = ord(c)
                 if code >= ord('A') and code <= ord('Z'):
-                    ret += chr(code + 32)
+                    ret.append(chr(code+32))
                 elif (code >= ord('a') and code <= ord('z')) or (code >= ord('0') and code <= ord('9')):
-                    ret += chr(code)
-            return ret
+                    ret.append(chr(code))
+            return ''.join(ret)
         s = convert(s)
         l, r = 0, len(s)-1
         while l < r:

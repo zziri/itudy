f40704cc90a82c24f493bbaef3c9b1891f1e124f
zziri <jihzang@naver.com>
2021-03-28 00:03:54 +0900

    326. Power of Three.java

---
+++ ps/zziri/leetcode/Problems/326. Power of Three.java
@@ -0,0 +1,30 @@
+// MEMO : Math Class의 method들은 부하가 큰 편이다(특히 pow같은 함수), 가끔은 Naive한 방법이 더 좋을 때도 있다. Parametric Search 방법도 고민해볼 필요가 있다
+
+// 17msec, 22.54%
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        
+        return n == 1;
+    }
+}
+
+// 21msec, 12.47%
+/*
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n==0) {
+            return false;
+        }
+
+        double x = Math.log(n)/Math.log(3);
+        return (long)n == (long)Math.pow(3, (long)Math.round(x));
+    }
+}
+*/

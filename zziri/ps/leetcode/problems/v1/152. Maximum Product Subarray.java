debbaf9a5f9d50527e0fd7617e9eb825d63dd10a
zziri <jihzang@naver.com>
2021-10-21 23:52:10 +0900

    leetcode: 152. Maximum Product Subarray.java
    최적화를 해보긴 했지만 완벽하게 이해는 못했다

--- zziri/ps/leetcode/problems/v1/152. Maximum Product Subarray.java
+++ zziri/ps/leetcode/problems/v1/152. Maximum Product Subarray.java
@@ -1,19 +1,28 @@
-// MEMO : 최적화 풀이 경험해보자
+// MEMO : 최적화를 해보긴 했지만 완벽하게 이해는 못했다
 
-// Runtime: 91 ms, faster than 6.52% of Java online submissions for Maximum Product Subarray.
-// Memory Usage: 38.6 MB, less than 81.42% of Java online submissions for Maximum Product Subarray.
+// Runtime: 1 ms, faster than 93.39% of Java online submissions for Maximum Product Subarray.
+// Memory Usage: 39.8 MB, less than 37.25% of Java online submissions for Maximum Product Subarray.
 
 class Solution {
     public int maxProduct(int[] nums) {
+        int product = 1;
         int ret = Integer.MIN_VALUE;
+
         for (int i=0; i<nums.length; i++) {
-            int temp = nums[i];
-            ret = Math.max(temp, ret);
-            for (int j=i+1; j<nums.length; j++) {
-                temp *= nums[j];
-                ret = Math.max(temp, ret);
-            }
+            product *= nums[i];
+            ret = Math.max(ret, product);
+            if (product == 0)
+                product = 1;
         }
+        product = 1;
+
+        for (int i=nums.length-1; i>=0; i--) {
+            product *= nums[i];
+            ret = Math.max(product, ret);
+            if (product == 0)
+                product = 1;
+        }
+        
         return ret;
     }
 }

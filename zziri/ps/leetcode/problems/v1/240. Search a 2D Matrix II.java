9c32a46d3768776c472b73be09e04d8c4114ec0e
zziri <jihzang@naver.com>
2021-12-02 21:05:23 +0900

    leetcode: 240. Search a 2D Matrix II.java
    간단하게 줄일 수 있는 아이디어!

--- zziri/ps/leetcode/problems/v1/240. Search a 2D Matrix II.java
+++ zziri/ps/leetcode/problems/v1/240. Search a 2D Matrix II.java
@@ -1,21 +1,27 @@
-// MEMO : 이렇게 푸는게 당연히 아니다. 다시 풀자
+// MEMO : 간단하게 줄일 수 있는 아이디어!
 
-// Runtime: 12 ms, faster than 20.07% of Java online submissions for Search a 2D Matrix II.
-// Memory Usage: 44.8 MB, less than 50.01% of Java online submissions for Search a 2D Matrix II.
+//Runtime: 4 ms, faster than 100.00% of Java online submissions for Search a 2D Matrix II.
+//Memory Usage: 44.7 MB, less than 59.83% of Java online submissions for Search a 2D Matrix II.
 
 class Solution {
+    private boolean isOut(int x, int y, int n, int m) {
+        return (x<0 || y<0 || x>=m || y>=n);
+    }
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if (matrix[i][j] == target) {
-                    return true;
-                }
+        int n = matrix.length, m = matrix[0].length;
+        int y = 0, x = m-1;
+
+        while (!isOut(x, y, n, m)) {
+            if (target < matrix[y][x]) {
+                x -= 1;
+            } else if (target > matrix[y][x]) {
+                y += 1;
+            } else {
+                return true;
             }
         }
-    
         return false;
     }
 }
+

09c79a02806c737dbac70dde831c224da3e7d4b0
zziri <jihzang@naver.com>
2022-04-06 00:49:49 +0900

    leetcode: 70. Climbing Stairs.java
    변수 3개로 메모이제이션

--- zziri/ps/leetcode/problems/70. Climbing Stairs.java
+++ zziri/ps/leetcode/problems/70. Climbing Stairs.java
@@ -1,15 +1,19 @@
-// MEMO : 변수 3개만으로 풀수 있으니까 다시 해보자
+// MEMO : 변수 3개로 메모이제이션
 
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
-// Memory Usage: 40.5 MB, less than 70.55% of Java online submissions for Climbing Stairs.
+// Memory Usage: 39 MB, less than 86.81% of Java online submissions for Climbing Stairs.
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[46];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        if (n <= 2) {
+            return n;
         }
-        return dp[n];
+        int first = 1, second = 2, ret = 0;
+        for (int i=3; i<=n; i++) {
+            ret = first + second;
+            first = second;
+            second = ret;
+        }
+        return ret;
     }
 }
+
